4f01fe442040e6ce6da8b4084b662efa32232095
Brianna Tanusi <btanusi@gmail.com>
2019-03-01 17:33:41 -0800

    changed max text to 240 char

--- moments/moment.model.js
+++ moments/moment.model.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const schema = new Schema({
     username: { type: String, required: true },
-    text: { type: String, required: true, maxlength: 200},			//allows for 200 characters
+    text: { type: String, required: true, maxlength: 240},			//allows for 240 characters
     emoji: { type: Number, min: 0, max: 7},			//allows for 8 emojis
 	createdDate: { type: Date, default: Date.now }
 });
