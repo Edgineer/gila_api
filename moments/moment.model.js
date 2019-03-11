dbe0f430372c8fb2038af35ff593b01affa8b012
jaythemishra <jaythemishra@gmail.com>
2019-03-10 18:24:17 -0700

    Modified moments model

--- moments/moment.model.js
+++ moments/moment.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-    username: { type: String, required: true },
+    username: { type: String, required: true, index: { unique: false }},
     text: { type: String, required: true, maxlength: 240},			//allows for 240 characters
     emoji: { type: Number, min: 0, max: 7},			//allows for 8 emojis
 	createdDate: { type: Date, default: Date.now }
